5e64a4ae01c26fd617c561ceac33eb5e9380a50e
ShriyanshRaut <rautshriyansh@gmail.com>
2026-03-29 11:32:08 +0530

    Remove duplicate src and logs from root

--- src/routes/order.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import authMiddleware from "../middlewares/auth.middleware.js";
-import { createOrder } from "../controllers/order.controller.js";
-
-const router = Router();
-
-router.post("/", authMiddleware, createOrder);
-
-export default router;
--- src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-const asyncHandler = (fn) =>{
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next)
-    }
-}
-console.log("ASYNC HANDLER WRAPPED");
-
-export default  asyncHandler 
